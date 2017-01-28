6e1a15bb7e2241e3baf56a876661f824b775aab6
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-01-28 09:31:51 +0000

    Adds class for accessing DevRant stuff

---
+++ mobile/src/main/java/io/github/skepter/devrantwear/DevRantAccessor.java
@@ -0,0 +1,16 @@
+package io.github.skepter.devrantwear;
+
+/**
+ * Created by Jorel on 28/01/2017.
+ */
+
+/*
+Some kind of class to access devRant
+JavaRant is available, but I would rather learn how to use
+GSON by myself now and then be able to apply it in the future.
+
+Also, JavaRant uses Java 8 with lambda expressions which are
+not compatible with Android (unfortunately).
+ */
+public class DevRantAccessor {
+}
